5f34fcf930370a6876193b868a8c9bbe363ff713
LifeScott <scottthushyanthan@outlook.it>
2019-03-13 17:21:26 +0100

    I click and it changes

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MenuItem } from 'src/app/model/menu-item';
 
 @Component({
   selector: 'app-menu',
@@ -8,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  menuList: MenuItem[] = [
+    {id: 1, descrizione: 'Home', selezionato: false},
+    {id: 2, descrizione: 'Lista', selezionato: false},
+    {id: 3, descrizione: 'Modifica', selezionato: false}
+  ];
+
+  @Output('showSection')
+  showSectionEvent: EventEmitter<number> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
     
+  }
+
+  showSection(id: number){
+    this.showSectionEvent.emit(id);
   }
 
 }
